6e8c88458b1c8e7702df7c50175b9a32d9d5722e
Alexandre Araújo <alexandre.araujo@iconenet.com.br>
2021-09-23 16:54:09 -0300

    readme

--- currrying.js
+++ currrying.js
@@ -10,3 +10,9 @@ console.log(soma2(2));
 console.log(soma2(3));
 console.log(soma2(4));
 console.log(soma2(5));
+
+const num = 11.12345;
+console.log(num.toFixed(3));
+
+
+console.log("teste".slice(0,-1));
