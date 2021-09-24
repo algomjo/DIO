cc23eaa2a6f2fc12300bf5f2653157473db6a42f
Alexandre Araújo <alexandre.araujo@iconenet.com.br>
2021-09-24 15:31:20 -0300

    23-09

--- currrying.js
+++ currrying.js
@@ -13,6 +13,20 @@ console.log(soma2(5));
 
 const num = 11.12345;
 console.log(num.toFixed(3));
-
-
+const undef
+console.log(typeof num);
 console.log("teste".slice(0,-1));
+
+let user ={
+    name: "Alexandre",
+    lastName: "Gomes",
+    work: "DEV"
+};
+
+
+
+console.log("User entries: ",Object.entries(user));
+console.log("User keys: ",Object.keys(user));
+Object.assign(user, {fullName: 'Alexandre Gomes'});
+console.log("User assign 1: ",user);
+console.log("User assign 2: ",Object.assign({},user, {age: 26}));
