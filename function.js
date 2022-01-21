af14fcd731e578a2d3eaadf912c264bf925c5d3d
Alexandre Gomes <algomjo@live.com>
2022-01-21 18:39:36 -0300

    21-01

--- function.js
+++ function.js
@@ -12,3 +12,22 @@ fn.prop = 'Posso criar Propriedades';
 console.log(fn());
 console.log(fn.prop);
 console.log(typeof fn());
+
+function sum (...args){
+    console.log(args);
+    console.log(arguments);
+}
+console.log(sum(5,5,5,2,3));
+
+const uniqueId = Symbol();
+
+console.log(uniqueId);
+
+const obj = {
+    values: [1,2,3,4],
+    [Symbol.iterator](){
+
+    }
+}
+
+console.log(obj);
